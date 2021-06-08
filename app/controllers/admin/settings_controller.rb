147ea2b6a542e3f345506d241316c3b563c22f54
chandrn7 <chandrn@umich.edu>
2021-06-08 19:13:36 -0400

    minor refactor of status_batch.rb; ensure that when private accounts are disallowed, all locked accounts are unlocked

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -15,6 +15,7 @@ module Admin
 
       if @admin_settings.save
         flash[:notice] = I18n.t('generic.changes_saved_msg')
+        check_private_accounts(settings_params)
         redirect_to edit_admin_settings_path
       else
         render :edit
@@ -26,5 +27,22 @@ module Admin
     def settings_params
       params.require(:form_admin_settings).permit(*Form::AdminSettings::KEYS)
     end
+
+    def check_private_accounts(params)
+      ApplicationRecord.transaction do
+        if params[:allow_private_accounts]
+          if !Setting.allow_private_accounts
+            Account.where(locked: true).reorder(nil).find_each do |account|
+              account.update!(locked: false)
+            end
+          end
+        end
+      end
+
+      true
+    rescue ActiveRecord::RecordInvalid
+      false
+    end
+    
   end
 end
